e0232a71441c6f2ad42409f67990838fb1ba5afd
Matheus-Du <duncanmatheus905@gmail.com>
2021-09-29 14:49:01 -0600

    fixed merge conflict with color

--- app/src/main/java/com/example/teamlab4/Shape.java
+++ app/src/main/java/com/example/teamlab4/Shape.java
@@ -3,9 +3,5 @@ package com.example.teamlab4;
 public abstract class Shape {
     int x;
     int y;
-<<<<<<< HEAD
-    String color = "blue";
-=======
     String colour = "turqouise";
->>>>>>> c0bc6f08aaf19ff8c32af1f1e75901915ff240b8
 }
